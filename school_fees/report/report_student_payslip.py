738c90c01c66101549f4e595aaf5ba7bbdda0cc2
Viltco <58694110+Viltco@users.noreply.github.com>
2019-12-24 13:08:42 +0300

    fees
    fees

---
+++ school_fees/report/report_student_payslip.py
@@ -0,0 +1,28 @@
+# See LICENSE file for full copyright and licensing details.
+
+from datetime import datetime
+from odoo import models, api
+
+
+class ReportStudentPayslip(models.AbstractModel):
+    _name = 'report.school_fees.student_payslip'
+    _description = "School Fees Payslip Report"
+
+    @api.multi
+    def get_month(self, indate):
+        out_date = indate.strftime('%B') + '-' + indate.strftime('%Y')
+        return out_date
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        student_payslip = self.env['student.payslip'].search([('id', 'in',
+                                                               docids)])
+        payslip_model = self.env['ir.actions.report'].\
+            _get_report_from_name('school_fees.student_payslip')
+        return {
+            'doc_ids': docids,
+            'doc_model': payslip_model.model,
+            'docs': student_payslip,
+            'data': data,
+            'get_month': self.get_month
+        }
